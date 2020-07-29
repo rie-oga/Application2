7092d0dbf8bcd86ece4e218558153e0cde4d922a
rie <kou.kotetsu@gmail.com>
2020-07-29 17:20:11 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b0ab8382312c793bc70bb0cf7d5a6c82d844f8e47e7b7905c9022b41d2ef8d88afbf2efb8d9fc90f14a498b6d51c76f86859b167757ef3513345066e8d421cdf'
